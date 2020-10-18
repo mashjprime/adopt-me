7d195aa2f8101fc51e80fa4d48ebe80597375bdc
Mash J Prime <mashjprime@gmail.com>
2020-10-18 19:54:58 +0800

    Converted to JSX

--- src/app.js
+++ src/app.js
@@ -3,24 +3,32 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Boss",
-      animal: "Dog",
-      breed: "Bull Arab",
-    }),
-    React.createElement(Pet, {
-      name: "Snowball",
-      animal: "Cat",
-      breed: "Beast",
-    }),
-    React.createElement(Pet, {
-      name: "Sasha",
-      animal: "Dog",
-      breed: "Corgi",
-    }),
-  ]);
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, {
+  //       name: "Boss",
+  //       animal: "Dog",
+  //       breed: "Bull Arab",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Snowball",
+  //       animal: "Cat",
+  //       breed: "Beast",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Sasha",
+  //       animal: "Dog",
+  //       breed: "Corgi",
+  //     }),
+  //   ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Boss" animal="Dog" breed="Bull Arab" />
+      <Pet name="Sasha" animal="Dog" breed="Corgi" />
+      <Pet name="Snowball" animal="Cat" breed="Beast" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
